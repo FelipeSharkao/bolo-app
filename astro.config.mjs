507b3d6572515996fa43a686e6a25e17f9f6f07d
Felipe Nascimento <felipenasgomes@gmail.com>
2023-04-06 12:22:32 -0300

    ci!: add SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+    output: "server",
     integrations: [solidJs(), tailwind()],
     vite: {
         ssr: {
