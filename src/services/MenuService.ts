b55f25b7bb36751db0ddea136e405e21ed40352f
Felipe Nascimento <felipenasgomes@gmail.com>
2023-07-18 22:14:36 -0300

    feat: load menu entries on GET

--- src/services/MenuService.ts
+++ src/services/MenuService.ts
@@ -1,3 +1,4 @@
+import type { MenuEntry, MenuItem, MenuSection } from "@/types/menu"
 import { AuthenticationError } from "@/utils/errors"
 
 import type { SessionService } from "./SessionService"
@@ -19,7 +20,47 @@ export class MenuService {
             .eq("id", id)
             .single()
 
-        return data
+        if (!data) {
+            console.log("menu not found", id)
+            return null
+        }
+
+        console.log("menu", id, data)
+
+        const loadEntries = async (parentId?: number): Promise<MenuEntry[]> => {
+            console.log("load entries", id, parentId)
+
+            const query = this.session.client
+                .from("menu_entries")
+                .select("id,title,description,min,max,price,is_section")
+                .order("id")
+
+            if (parentId == null) {
+                query.eq("menu_id", id)
+            } else {
+                query.eq("parent_id", parentId)
+            }
+
+            const { data } = await query
+
+            console.log("entries", id, parentId, data)
+
+            return Promise.all(
+                (data || []).map(async (entry) => {
+                    if (entry.is_section) {
+                        return {
+                            ...entry,
+                            entries: await loadEntries(entry.id),
+                            type: "section",
+                        } as MenuSection
+                    }
+
+                    return { ...entry, type: "item" } as MenuItem
+                }),
+            )
+        }
+
+        return { ...data, sections: (await loadEntries()) as MenuSection[] }
     }
 
     async find(skip: number, limit: number) {
@@ -32,7 +73,7 @@ export class MenuService {
 
         const { data } = await this.session.client
             .from("menus")
-            .select("id,title,description")
+            .select("id,title")
             .eq("user_id", uid)
             .range(skip, skip + limit)
             .order("updated_at", { ascending: false })
